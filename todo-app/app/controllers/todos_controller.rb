5fc8499a1a1a1aec225ffb64ec7d61f2c2bb73ba
Luke Gill <bagrad007@gmail.com>
2021-04-26 15:01:29 -0400

    new challenge

--- todo-app/app/controllers/todos_controller.rb
+++ todo-app/app/controllers/todos_controller.rb
@@ -1,9 +1,14 @@
 class TodosController < ApplicationController
+  def index
+    todos = Todo.all
+    render json: todos
+  end
 
-    def index
-        todos = Todo.all
-        render json: todos
-    end
+  def create
+    render json: Todo.create(todo_params)
+  end
 
-    
+  def todo_params
+    params.require(:todo).permit(:content)
+  end
 end
